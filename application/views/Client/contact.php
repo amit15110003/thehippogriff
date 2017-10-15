d5cabf3e2c9e533f957f44aee7321d8507067e63
rrptm <raviranjan.ptm@gmail.com>
2017-10-16 03:41:26 +0530

    gj
    knn

---
+++ application/views/Client/contact.php
@@ -0,0 +1,38 @@
+<div class="spacer"></div>
+<div id="startchange"></div>
+<div class="page-header">
+		<h1>Contact us</h1>
+</div>
+<div class="container">
+	<div class="col-md-offset-2 col-md-9 col-md-offset-1">
+			<h5>
+				We would love to hear from you!
+			</h5>
+		<div>
+			<span class="glyphicon glyphicon-home" style="padding-top: 40px; font-size: 15px;" aria-hidden="true">           	<b> ADDRESS</b>  B74, Chattarpur Extension, New Delhi</span>
+			<div><span class="glyphicon glyphicon-earphone" style="padding-top: 40px; font-size: 15px;" aria-hidden="true">           	<b> PHONE
+</b>  +919654925861</span></div>
+			
+			<div><span class="glyphicon glyphicon-home" style="padding-top: 40px; padding-bottom: 50px; font-size: 15px;" aria-hidden="true">           	<b> EMAIL</b>  
+info@thehippogriff.com</span></div>
+			
+
+		</div>
+		</div>
+
+<div class="container col-md-offset-8 col-md-4" style="background-color: #7c7c80">
+<h2>Get in Touch</h2>
+<div style="padding-top: 25px;"><input type="text" class="form-control" placeholder="Name" ">
+</div>
+<div style="padding-top: 25px;"><input type="text" class="form-control" placeholder="Email" ">
+</div>
+<div style="padding-top: 25px;"><input type="text" class="form-control" placeholder="Phone Number" ">
+</div>
+<div style="padding-top: 25px; padding-bottom: 25px;"><input type="text" class="form-control" placeholder="Subject" ">
+<div style="padding-top: 25px; padding-bottom: 35px padding-top: 125px;"><input type="text" class="form-control" placeholder="Message" ">
+<button class="theme-btn" style="padding-bottom: 25px;"> SEND MESSAGE</button>
+</div>
+</div>
+
+	
+</div>
