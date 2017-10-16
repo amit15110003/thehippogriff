15239f7b9933700ef38cadaae0960cc6b4b84533
rrptm <raviranjan.ptm@gmail.com>
2017-10-17 02:59:17 +0530

    cxvx
    vxvzxxvx

---
+++ application/views/Client/profile.php
@@ -0,0 +1,53 @@
+<div class="spacer"></div>
+<div id="startchange"></div>
+<div class="page-header">
+		<h1>Profile</h1>
+</div>
+<div class="container">
+	<h3><b>Account Details</b></h3>
+</div>
+<div class="col-md-offset-2 col-md-8 col-md-offset-2">
+<form class="">
+  <div class=" col-md-6">
+    <label for="exampleInputName2">First Name </label>
+    <input type="text" class="theme-form" id="exampleInputName2" placeholder="">
+  </div>
+  <div class="col-md-6">
+    <label for="exampleInputEmail2">Last Name</label>
+    <input type="email" class="theme-form" id="exampleInputEmail2" placeholder="">
+  </div>
+  <div class=" col-md-12">
+    <label for="exampleInputEmail1";>Email address</label>
+    <input type="email" class="theme-form" id="exampleInputEmail1" placeholder="">
+    <br>
+    </div>
+</form>
+</div>
+<div class="container"></div>
+<div class="container">
+	<h3><b>Password Change</b></h3>
+	<br>
+</div>
+<div class="col-md-offset-2 col-md-8 col-md-offset-2">
+<form class="">
+    <div class=" col-md-12">
+    <label for="exampleInputEmail1";>Current Password (leave blank to leave unchanged)</label>
+    <input type="Password" class="theme-form" id="exampleInputEmail1">
+  </div>
+  <div class=" col-md-12">
+    <label for="exampleInputEmail1";>New Password (leave blank to leave unchanged)</label>
+    <input type="Password" class="theme-form" id="exampleInputEmail1">
+  </div>
+  <div class=" col-md-12">
+    <label for="exampleInputEmail1";>Confirm New Password</label>
+    <input type="Password" class="theme-form" id="exampleInputEmail1">
+  </div>
+</form>
+</div>  
+
+
+<div class="container col-md-offset-4 col-md-4" >
+	<br>
+	<button class="theme-btn-lg col-sm-12 col-md-12" > PROCEED TO CHECKOUT</button>
+</div>
+<div class="container"></div>
