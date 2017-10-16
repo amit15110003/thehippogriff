5c2c79f6b64336b921afb236d1dcbe3751b53c3a
amit15110003 <amitanand.kvj.2@gmail.com>
2017-10-16 20:37:26 +0530

    sss

--- application/views/Client/product.php
+++ application/views/Client/product.php
@@ -7,14 +7,35 @@
 	<div class="col-md-6 text-center">
 		<h1>Abstract Colours</h1>
 		<h3>High quality prints on both sides of the sleeve. Waterproof and cushioned material with a smooth zipper. Available in 13 inches and 15 inches.  The artwork on this sleeve is composed by Aleksandr Volodchenkov.</h3>
+		<form class="">
+			<div class="form-inline col-md-8 col-md-offset-2 col-xs-12">
+				<div class="col-xs-2 col-md-2">
+					<button type="button" class="btn theme-btn" aria-label="Left Align">
+					  	<span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+					</button>
+				</div>
+				<div class="col-xs-8 col-md-8">
+					<input type="number" class="form-control" id="exampleInputAmount" value="1">
+				</div>
+				<div class="col-xs-2 col-md-2">
+					<button type="button" class="btn theme-btn" aria-label="Left Align">
+	  				<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+			<div class="col-md-3"></div>
+			<div class="row">
+				<button type="submit" class="btn theme-btn-lg"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add To Cart</button>
+			</div>
+		</form>
 	</div>
 </div>
 <div class="container product-details text-center">
-	 <ul class="nav nav-tabs" role="tablist">
-	    <li role="presentation" class="active"><a href="#info" aria-controls="info" role="tab" data-toggle="tab">Additional Information</a></li>
-	    <li role="presentation"><a href="#comment" aria-controls="coment" role="tab" data-toggle="tab">Comment</a></li>
+	 <ul class="info-swap text-center" role="">
+	    <li class="active"><a href="#info" aria-controls="info" role="tab" data-toggle="tab">Additional Information</a></li>
+	    <li><a href="#comment" aria-controls="coment" role="tab" data-toggle="tab">Comment</a></li>
 	  </ul>
-
+	  <hr>
 	  <!-- Tab panes -->
 	  <div class="tab-content">
 	    <div role="tabpanel" class="tab-pane active" id="info">
