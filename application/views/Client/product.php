2a185c82bd83c210f984cf091c626448a441fba6
amit15110003 <amitanand.kvj.2@gmail.com>
2017-10-16 21:54:36 +0530

    ss

--- application/views/Client/product.php
+++ application/views/Client/product.php
@@ -6,24 +6,20 @@
 	</div>
 	<div class="col-md-6 text-center">
 		<h1>Abstract Colours</h1>
-		<h3>High quality prints on both sides of the sleeve. Waterproof and cushioned material with a smooth zipper. Available in 13 inches and 15 inches.  The artwork on this sleeve is composed by Aleksandr Volodchenkov.</h3>
+		<h4>High quality prints on both sides of the sleeve. Waterproof and cushioned material with a smooth zipper. Available in 13 inches and 15 inches.  The artwork on this sleeve is composed by Aleksandr Volodchenkov.</h4>
 		<form class="">
-			<div class="form-inline col-md-8 col-md-offset-2 col-xs-12">
-				<div class="col-xs-2 col-md-2">
-					<button type="button" class="btn theme-btn" aria-label="Left Align">
-					  	<span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
-					</button>
-				</div>
-				<div class="col-xs-8 col-md-8">
-					<input type="number" class="form-control" id="exampleInputAmount" value="1">
-				</div>
-				<div class="col-xs-2 col-md-2">
-					<button type="button" class="btn theme-btn" aria-label="Left Align">
-	  				<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-					</button>
-				</div>
+			<div class="row">
+			<div class="text-center quantity">
+				<button class="decrease">
+						<span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+				</button>
+				<input type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="[0-9]*" inputmode="numeric">
+				<button class="increase">
+					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+				</button>
 			</div>
-			<div class="col-md-3"></div>
+			</div>
+			<br><br>
 			<div class="row">
 				<button type="submit" class="btn theme-btn-lg"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add To Cart</button>
 			</div>
